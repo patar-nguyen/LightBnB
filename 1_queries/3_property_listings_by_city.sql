4f84d0f42882a349a873c1c18eb8137e26b08343
Patrick Nguyen <patar.nguyen@gmail.com>
2022-02-09 17:24:44 -0500

    Add Select Statements

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT properties.id, properties.title, properties.cost_per_night, AVG(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON property_id = properties.id
+WHERE city LIKE '%ancouve%'
+GROUP BY properties.id 
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
